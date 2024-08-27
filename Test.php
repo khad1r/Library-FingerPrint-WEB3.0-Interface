96d8f2385a63d68b1fbf0ae25fa12cac09100ce7
Kadir <60246846+khad1r@users.noreply.github.com>
2024-08-27 12:15:39 +0700

    Add Bruno HTTP and Release Composer Package

--- Test.php
+++ Test.php
@@ -1,15 +1,49 @@
 <?php
 
+require 'vendor/autoload.php';
+// require_once './AttendanceLog.php';
+require 'src/ZtecoWeb3Api.php';
 
-require_once './AttendanceLog.php';
+use khad1r\webfingerprint\ZtecoWeb3Api;
 
-$ip = "36.95.106.105";
-$port = "45179";
-$startDate = '2023-03-30';
-$endDate = '2023-04-04';
-$uidList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
-$AttndLog = new AttendanceLog($ip, $port);
-$AttndLog->prepare($startDate, $endDate, $uidList);
-$AttndLog->execute();
-$resultLog = $AttndLog->getResult();
-var_dump($resultLog);
+$AttndLog = new ZtecoWeb3Api(getenv('URL_ZTECKO'), getenv('USER_ZTECKO'), getenv('PASS_ZTECKO'));
+$startDate = date('Y-m-01');
+$endDate = date('Y-m-t');
+$uidList = [/* 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 */];
+function getAttendance()
+{
+  global $AttndLog;
+  global $uidList;
+  global $startDate;
+  global $endDate;
+  $resultLog = $AttndLog->getAttendance($startDate, $endDate, $uidList);
+  $formatedResult = $AttndLog->formatAttendanceByDate($resultLog);
+  $formatedResult2 = $AttndLog->formatAttendanceByID($resultLog);
+  echo 'Group By Date';
+  echo json_encode($formatedResult, JSON_PRETTY_PRINT);
+  echo 'Group By ID';
+  echo json_encode($formatedResult2, JSON_PRETTY_PRINT);
+}
+function searchSomeone()
+{
+  global $AttndLog;
+  global $uidList;
+  $ID15 = $AttndLog->getByID(15);
+  echo json_encode($ID15, JSON_PRETTY_PRINT);
+  $uidList = [$ID15['uid']];
+  getAttendance();
+}
+
+function getAllUsers()
+{
+  global $AttndLog;
+  global $uidList;
+  $workers = $AttndLog->getUsers(100);
+  echo json_encode($workers, JSON_PRETTY_PRINT);
+  $uidList = array_column($workers, 'uid');
+  getAttendance();
+}
+echo 'Search By ID ';
+searchSomeone();
+echo 'Get All Data';
+getAllUsers();
--- src/ZtecoWeb3Api.php
+++ src/ZtecoWeb3Api.php
@@ -1,137 +1,385 @@
 <?php
 
-class AttendanceLog
-{
-    private $ATTENDANCE_MACHINE_IP;
-    private $ATTENDANCE_MACHINE_PORT;
-    private $ATTENDANCE_MACHINE_WEB3_PORTAL_URL;
-    private $CURL;
-    private $CURL_BODY;
-    private $CURL_METHOD = "POST";
-    private $CURL_RESPONSE;
-    private $CURL_ERROR;
+namespace khad1r\webfingerprint;
 
+class ZtecoWeb3Api
+{
+    private $url;
+    private $username;
+    private $password;
+    private $curl;
+    private $sessionId;
+    private $curlResponse;
+    private $curlOpt;
+    public $curlError;
+    private const VALIDLOGINRESP = "<HTML><HEAD><TITLE></TITLE><METAhttp-equiv=Content-Typecontent='text/html;'><METAhttp-equiv=Cache-controlcontent=no-cache><METAhttp-equiv=Pragmacontent=no-cache><LINKhref='../css/Secutime.css'type=text/cssrel=stylesheet><METAcontent='MSHTML6.00.2900.3562'name=GENERATOR></HEAD><FRAMESETborder=0frameSpacing=0rows=37,*frameBorder=NOcols=*><FRAMEname=headersrc='/csl/header'noResizescrolling=no><FRAMESETborder=0frameSpacing=0rows=*frameBorder=NOcols=180,*><FRAMEname=menusrc='/csl/menu'noResizescrolling=auto><FRAMEname=contentsrc='/csl/desktop'></FRAMESET></FRAMESET></HTML>";
     /**
-     * AttendanceLog constructor.
+     * ZtecoWeb3 constructor.
      *
-     * @param string $ip The IP address of the attendance machine
-     * @param string $port The port number to connect to the attendance machine
+     * @param string $url The URL of the attendance machine
+     * @param string $username The username for the attendance machine
+     * @param string $password The password for the attendance machine
+     * @throws Exception
      */
-    public function __construct(string $ip, string $port)
+    public function __construct(string $url, string $username, string $password)
     {
-        $this->ATTENDANCE_MACHINE_IP = $ip;
-        $this->ATTENDANCE_MACHINE_PORT = $port;
-        $this->ATTENDANCE_MACHINE_WEB3_PORTAL_URL = "http://$this->ATTENDANCE_MACHINE_IP:$this->ATTENDANCE_MACHINE_PORT/form/Download";
+        $this->url = $url;
+        $this->username = $username;
+        $this->password = $password;
+        $this->getSessionId();
+        // usleep(50000); // Sleep for 10 milliseconds
+        if (!$this->login()) {
+            throw new \Exception("Unable to login to the attendance machine");
+        }
     }
 
     /**
-     * Prepares the cURL request to download attendance logs for a specific user and date range.
+     * Get default cURL options.
      *
-     * @param string $startDate The start date of the attendance log in yyyy-mm-dd format
-     * @param string $endDate The end date of the attendance log in yyyy-mm-dd format
-     * @param mixed $uid The user ID(s) of the user(s) whose attendance logs are to be downloaded
-     * @return $this The AttendanceLog object instance
+     * @return array
      */
-    public function prepare(string $startDate, string $endDate, mixed $uid)
+    private function contructCurlOpt(array $options = [])
     {
-        $this->CURL = curl_init();
+        return [
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => $options[CURLOPT_CUSTOMREQUEST] ?? 'GET',
+            CURLOPT_HTTPHEADER => $options[CURLOPT_HTTPHEADER] ?? [
+                "content-type: text/plain",
+                "cookie: SessionID={$this->sessionId}"
+            ]
+        ] + $options;
+    }
+    /**
+     * Prepares the cURL request with the given options.
+     *
+     * @param array $options Additional cURL options
+     * @return $this
+     */
+    private function prepareCurl(array $options = [])
+    {
+        $this->curl = curl_init();
+        $this->curlOpt = $this->contructCurlOpt($options);
+        curl_setopt_array($this->curl, $this->curlOpt);
+        return $this;
+    }
+
+    /**
+     * Executes the cURL request and captures the response.
+     *
+     * @return $this
+     */
+    private function executeCurl()
+    {
+        $this->curlResponse = curl_exec($this->curl);
+        $err = curl_error($this->curl);
+        $this->curlError = ($err) ? "cURL Error: $err" : false;
+        curl_close($this->curl);
+        return $this;
+    }
+
+
+    /**
+     * Encodes data for URL-encoded requests.
+     *
+     * @param mixed  $data The data to be encoded
+     * @return string The URL-encoded string
+     */
+    private function urlEncode(mixed $data)
+    {
+        return preg_replace(
+            '/uid%5B\d+%5D=/',
+            'uid=',
+            http_build_query($data, '', '&', PHP_QUERY_RFC3986)
+        );
+    }
+
+    /**
+     * Validates the date format.
+     *
+     * @param string $date Date in yyyy-mm-dd format
+     * @return bool
+     */
+    private function isValidDate(string $date)
+    {
+        return preg_match('/^\d{4}-\d{2}-\d{2}$/', $date);
+    }
+
+    /**
+     * Formats the raw result into an array of logs.
+     *
+     * @param string $data Raw data
+     * @return array
+     */
+    private function formatResult(string $data)
+    {
+        $lines = explode("\n", $data);
+        $result = [];
+
+        foreach ($lines as $line) {
+            if (empty($line)) continue;
+            $values = explode("\t", $line);
+            $result[] = [
+                "id" => $values[0],
+                "name" => $values[1],
+                "dateTime" => $values[2],
+                "status" => $values[3],
+                "verification" => $values[4],
+            ];
+        }
+
+        usort($result, function ($a, $b) {
+            return $a['name'] === $b['name']
+                ? strtotime($a['dateTime']) - strtotime($b['dateTime'])
+                : strcmp($a['name'], $b['name']);
+        });
+
+        return $result;
+    }
+    /**
+     * Formats HTML response into an array of user data.
+     *
+     * @param string $html HTML response from the cURL request
+     * @return array Array of user data
+     */
+    private function formatHTMLResult(string $html)
+    {
+        // Initialize DOMDocument and load HTML
+        $dom = new \DOMDocument();
+        libxml_use_internal_errors(true); // Suppress errors for malformed HTML
+        $dom->loadHTML($html);
+        libxml_clear_errors();
+
+        // Initialize DOMXPath to query HTML
+        $xpath = new \DOMXPath($dom);
+        $rows = $xpath->query('//div[@id="cc"]//table//tr');
+        $data = [];
+        // Process each row and extract data
+        foreach ($rows as $row) {
+            $cells = $xpath->query('.//td', $row);
+            $checkbox = $xpath->query('.//input[@type="checkbox" and @name="uid"]', $row)->item(0);
+            $data[trim($cells[2]->textContent) ?? null] = [
+                'uid' => $checkbox ? intval($checkbox->getAttribute('value')) : '',
+                'Departement' => trim($cells[1]->textContent) ?? '',
+                'ID Number' => trim($cells[2]->textContent) ?? '',
+                'Name' => trim($cells[3]->textContent) ?? '',
+                'Card' => trim($cells[4]->textContent) ?? '',
+                'Group' => trim($cells[5]->textContent) ?? '',
+                'Privilege' => trim($cells[6]->textContent) ?? '',
+            ];
+        }
+
+        return $data;
+    }
+
+    /**
+     * Gets the session ID from the server.
+     *
+     * @return $this
+     */
+    private function getSessionId()
+    {
+        // Prepare and execute the cURL request
+        $this->prepareCurl([
+            CURLOPT_URL => $this->url,
+            CURLOPT_HEADER => true,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => []
+        ])->executeCurl();
+
+        $header_size = curl_getinfo($this->curl, CURLINFO_HEADER_SIZE);
+        $header = substr($this->curlResponse, 0, $header_size);
+        // Check and extract SessionID from the 'Set-Cookie' header
+        if ($header && preg_match('/SessionID=([^;]+)/', $header, $sessionIdMatch)) {
+            $this->sessionId = $sessionIdMatch[1];
+        }
+        return $this;
+    }
+
+    /**
+     * Logs into the attendance machine.
+     *
+     * @return bool
+     */
+    private function login()
+    {
+        $data = [
+            'username' => $this->username,
+            'userpwd' => $this->password,
+        ];
+        $curlBody = $this->urlEncode($data);
+        $this->prepareCurl([
+            CURLOPT_URL => "{$this->url}/csl/check",
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => $curlBody,
+        ])->executeCurl();
+        return (!$this->curlError && (self::VALIDLOGINRESP == preg_replace('/\s+/', '', $this->curlResponse)));
+    }
+    /**
+     * Creates a new user with the given ID, name, and pin.
+     *
+     * @param int $id User ID (must be a number up to 9999)
+     * @param string $name User name
+     * @param int $pin User pin (must be a number with up to 6 digits)
+     * @return bool True on success, false on failure
+     */
+    public function create(int $id, string $name, int $pin)
+    {
+        // Validate ID and pin
+        if (!preg_match('/^\d{1,9}$/', $id)) {
+            throw new \InvalidArgumentException("ID must be a number up to 999999999.");
+        }
+        if (!preg_match('/^.{1,8}$/', $name)) {
+            throw new \InvalidArgumentException("Name must be at most 8 characters long.");
+        }
+        if (!preg_match('/^\d{1,6}$/', $pin)) {
+            throw new \InvalidArgumentException("Pin must be a number with up to 6 digits.");
+        }
+
+        // Prepare the data and cURL options
+        $data = [
+            'upin' => 'NULL',
+            'upin2' => $id,
+            'uname' => $name,
+            'uprivilege' => 0,
+            'upwd' => $pin,
+            'ucard' => 0,
+        ];
+        $this->prepareCurl([
+            CURLOPT_URL => "{$this->url}/csl/user?action=save&id=add",
+            CURLOPT_POSTFIELDS => $this->urlEncode($data),
+        ])->executeCurl();
+
+        // Return success status
+        return !$this->curlError;
+    }
+
+    /**
+     * Deletes a user by their UID.
+     *
+     * @param int|string $uid User UID to delete
+     * @return bool True on success, false on failure
+     */
+    public function delete(int|string $uid)
+    {
+        // Prepare the data and cURL options
+        $data = ['uid' => $uid];
+        $this->prepareCurl([
+            CURLOPT_URL => "{$this->url}/csl/user?action=del",
+            CURLOPT_POSTFIELDS => $this->urlEncode($data),
+        ])->executeCurl();
+
+        // Return success status
+        return !$this->curlError;
+    }
+    /**
+     * Retrieves user data with a limit on the number of users.
+     *
+     * @param int $count Number of users to retrieve (default is 100)
+     * @return array|bool User data array on success, false on failure
+     */
+    public function getUsers(int $count = 100)
+    {
+        // Prepare cURL options for GET request
+        $this->prepareCurl([
+            CURLOPT_URL => "{$this->url}/csl/user?last=$count",
+        ])->executeCurl();
+
+        // Return formatted result or false on error
+        return !$this->curlError ? $this->formatHTMLResult($this->curlResponse) : false;
+    }
+    /**
+     * Retrieves user data by user ID.
+     *
+     * @param int|string $id User ID to retrieve
+     * @return array|bool User data array on success, false on failure
+     */
+    public function getByID(int|string $id)
+    {
+        // Prepare cURL options for GET request
+        $this->prepareCurl([
+            CURLOPT_URL => "{$this->url}/csl/user?uid=$id",
+        ])->executeCurl();
+
+        // Return formatted result or false on error
+        return !$this->curlError
+            ? $this->formatHTMLResult($this->curlResponse)[$id] ?? false
+            : false;
+    }
+
+    /**
+     * Gets the attendance log for a specific date range and user.
+     *
+     * @param string $startDate Start date (yyyy-mm-dd)
+     * @param string $endDate End date (yyyy-mm-dd)
+     * @param mixed $uid User ID(s)
+     * @return array|false
+     * @throws InvalidArgumentException
+     */
+    public function getAttendance(string $startDate, string $endDate, $uid)
+    {
+        if (!$this->isValidDate($startDate) || !$this->isValidDate($endDate)) {
+            throw new \InvalidArgumentException("Invalid date format. Use yyyy-mm-dd.");
+        }
 
         $data = [
             'sdate' => $startDate,
             'edate' => $endDate,
             'uid' => $uid,
         ];
-        $this->CURL_BODY = $this->_urlEncode($data);
-
-        curl_setopt_array($this->CURL, [
-            CURLOPT_PORT => $this->ATTENDANCE_MACHINE_PORT,
-            CURLOPT_URL => $this->ATTENDANCE_MACHINE_WEB3_PORTAL_URL,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => $this->CURL_METHOD,
-            CURLOPT_POSTFIELDS => $this->CURL_BODY,
-            CURLOPT_HTTPHEADER => [
-                "Accept: */*",
-            ],
-        ]);
-        return $this;
+        $curlBody = $this->urlEncode($data);
+        $this->prepareCurl([
+            CURLOPT_URL => "{$this->url}/form/Download",
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => $curlBody,
+        ])->executeCurl();
+        return $this->curlError ? false : $this->formatResult($this->curlResponse);
     }
 
     /**
-     * Executes the cURL request and stores the response in the CURL_RESPONSE property of the object.
-     * Sets the CURL_ERROR property if there was an error in the cURL request.
+     * Format data from getAttendance with grouped by Date.
      *
-     * @return $this The AttendanceLog object instance
+     * @param array $data from output of getAttendance
+     * @return array Formatted data 
      */
-    public function execute()
+    public function formatAttendanceByDate(array $data)
     {
-        $this->CURL_RESPONSE = curl_exec($this->CURL);
-        $err = curl_error($this->CURL);
-        $this->CURL_ERROR = ($err) ? "cURL Error #:$err" : false;
-        curl_close($this->CURL);
-        return $this;
-    }
+        $grouped = [];
 
-    /** 
-     * This function returns the formatted result of the cURL request, which is an array of attendance logs.
-     * If there was an error with the cURL request, it returns the error message instead.
-     *
-     * @return array|string The formatted result of the cURL request or the error message
-     */
-    public function getResult()
-    {
-        return ($this->CURL_ERROR) ? $this->CURL_ERROR : $this->_formatResult($this->CURL_RESPONSE);
-    }
+        foreach ($data as $entry) {
+            $date = substr($entry['dateTime'], 0, 10); // Extract date (YYYY-MM-DD)
+            $time = substr($entry['dateTime'], 11, 8);  // Extract time (HH:MM:SS)
+            $id = $entry['id'];
 
-    private function _urlEncode(mixed $data)
-    {
-        /** 
-         * This is a private function that encodes the data array into a URL-encoded string for use in the cURL request body.
-         *
-         * @param mixed $data The data to be encoded
-         *
-         * @return string The URL-encoded string
-         */
-        $body = http_build_query($data, '', '&', PHP_QUERY_RFC3986);
-
-        $body = str_replace('uid%5B', 'uid=', $body);
-        $body = str_replace('%5D', '', $body);
-        return $body;
-    }
-
-    private function _formatResult(string $data)
-    {
-        /**
-         * This is a private function that takes in the raw response data from the cURL request and formats it into an array
-         * of attendance logs sorted by name and date/time.
-         *
-         * @param string $data The raw response data from the cURL request
-         *
-         * @return array The formatted array of attendance logs
-         */
-        $lines = explode("\n", $data);
-        $result = array();
-
-        foreach ($lines as $line) {
-            if (empty($line)) continue;
-            $values = explode("\t", $line);
-            $result[] = array(
-                "uid" => $values[0],
-                "name" => $values[1],
-                "Date Time" => $values[2],
-                "Status" => $values[3],
-                "Verification" => $values[4]
-            );
+            // Add entry to grouped data
+            $grouped[$date][$id][$time] = $entry;
         }
-        usort($result, function ($a, $b) {
-            if ($a['name'] == $b['name']) {
-                return strtotime($a['Date Time']) - strtotime($b['Date Time']);
-            }
-            return $a['name'] < $b['name'] ? -1 : 1;
-        });
-        return $result;
+
+        return $grouped;
+    }
+
+    /**
+     * Format data from getAttendance with grouped by ID.
+     *
+     * @param array $data from output of getAttendance
+     * @return array Formatted data 
+     */
+    public function formatAttendanceByID(array $data)
+    {
+        $grouped = [];
+
+        foreach ($data as $entry) {
+            $date = substr($entry['dateTime'], 0, 10); // Extract date (YYYY-MM-DD)
+            $time = substr($entry['dateTime'], 11, 8);  // Extract time (HH:MM:SS)
+            $id = $entry['id'];
+
+            // Add entry to grouped data
+            $grouped[$id][$date][$time] = $entry;
+        }
+
+        return $grouped;
     }
 }
